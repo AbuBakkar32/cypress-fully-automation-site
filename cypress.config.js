8eab0204c1ea1365ced161d4dc288e57de48a135
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-11 13:05:50 +0600

    contact page test

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,21 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+    //screenshotsFolder: 'cypress/screenshots',
+    //videosFolder: 'cypress/videos',
+    reporter: 'mochawesome',
+    //video: true,
+    //screenshotOnRunFailure: true,
+
     env: {
-        codeCoverage: {
-            url: 'http://localhost:3000/__coverage__',
-        },
+        // codeCoverage: {
+        //     url: 'http://localhost:3000/__coverage__',
+        // },
     }, projectId: '5r89t6', e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
-            require('@cypress/code-coverage/task')(on, config)
-            on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+            //require('@cypress/code-coverage/task')(on, config)
+            //on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
             return config
         },
     },
