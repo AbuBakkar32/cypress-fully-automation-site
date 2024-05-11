55de47d827fef45b0f32cf6283a418f1747745f5
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-11 19:06:47 +0600

    user login

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,42 @@ const {beforeRunHook, afterRunHook} = require('cypress-mochawesome-reporter/lib'
 
 module.exports = defineConfig({
     screenshotsFolder: 'cypress/screenshots',
+    screenshotOnRunFailure: true,
+    screenshotOnAssertFailure: true,
+    trashAssetsBeforeRuns: false,
     screenshots: true,
     videosFolder: 'cypress/videos',
-    video: true,
+    video: false,
+    downloadsFolder: 'cypress/downloads',
+    fixturesFolder: 'cypress/fixtures',
+    viewportHeight: 660,
+    viewportWidth: 1000,
+    waitForAnimations: true,
+    watchForFileChanges: true,
     //screenshotOnRunFailure: false,
-    reporter: 'cypress-mochawesome-reporter', reporterOptions: {
+    /*reporter: 'cypress-mochawesome-reporter', reporterOptions: {
         charts: true,
         reportPageTitle: 'custom-title',
+        reportDir: 'cypress/reports',
         embeddedScreenshots: true,
         inlineAssets: true,
-        saveAllAttempts: false,
+        saveAllAttempts: true,
+    },*/
+
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+        reporterEnabled: "cypress-mochawesome-reporter, mocha-junit-reporter",
+        mochaJunitReporterReporterOptions: {
+            mochaFile: "cypress/reports/junit/results-[hash].xml"
+        },
+        cypressMochawesomeReporterReporterOptions: {
+            charts: true,
+            reportPageTitle: "custom-title",
+            reportDir: 'cypress/reports',
+            embeddedScreenshots: true,
+            inlineAssets: true,
+            saveAllAttempts: true,
+        }
     },
 
     env: {}, projectId: '5r89t6', e2e: {
