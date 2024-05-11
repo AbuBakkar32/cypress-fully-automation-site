d1b589ad794f5aeb85e8038a564c0c5266abca31
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-11 12:03:58 +0600

    contact page test

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '5r89t6',
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
---
+++ cypress/e2e/contact_us.cy.js
@@ -0,0 +1,41 @@
+describe('Contact Us Form', () => {
+    it('Contact Us Form validation checking....', () => {
+        // Step 1: Launch browser
+        // Cypress automatically launches the browser
+
+        // Step 2: Navigate to url 'http://automationexercise.com'
+        cy.visit('http://automationexercise.com');
+
+        // Step 3: Verify that home page is visible successfully
+        cy.url().should('include', 'automationexercise');
+
+        // Step 4: Click on 'Contact Us' button
+        cy.contains('Contact us').click();
+
+        // Step 5: Verify 'GET IN TOUCH' is visible
+        cy.get('#contact-page > div.row > div.col-sm-8 > div > h2').should('contain', 'Get In Touch');
+
+        // Step 6: Enter name, email, subject and message
+        cy.get('[data-qa="name"]').type('Test User');
+        cy.get('[data-qa="email"]').type('testuser@example.com');
+        cy.get('[data-qa="subject"]').type('Test Subject');
+        cy.get('[data-qa="message"]').type('Test Message');
+
+        // Step 7: Upload file
+        const fileName = 'C:\\Users\\ABS RAKIB\\Videos\\Abu Bakkar Siddikk-CV.pdf';
+        cy.get('input[type=file]').selectFile(fileName);
+
+        // Step 8: Click 'Submit' button
+        cy.get('[data-qa="submit-button"]').click();
+
+        // Step 9: Click OK button
+        cy.on('window:confirm', () => true);
+
+        // Step 10: Verify success message 'Success! Your details have been submitted successfully.' is visible
+        cy.get('.status ').should('contain', 'Success! Your details have been submitted successfully.');
+
+        // Step 11: Click 'Home' button and verify that landed to home page successfully
+        cy.get('#form-section > a > span > i').click();
+        cy.url().should('include', 'automationexercise');
+    });
+});
