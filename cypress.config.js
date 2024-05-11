4c703400bdd7d5207b58fa51b8e6a45ec572eeca
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-11 17:39:32 +0600

    user login

--- cypress.config.js
+++ cypress.config.js
@@ -2,24 +2,21 @@ const {defineConfig} = require("cypress");
 const {beforeRunHook, afterRunHook} = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
-    //screenshotsFolder: 'cypress/screenshots',
-    //videosFolder: 'cypress/videos',
-    //reporter: 'mochawesome',
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
     reporter: 'cypress-mochawesome-reporter', reporterOptions: {
         charts: true,
         reportPageTitle: 'custom-title',
         embeddedScreenshots: true,
         inlineAssets: true,
         saveAllAttempts: false,
-    }, //video: true,
-    //screenshotOnRunFailure: true,
+    },
+    video: true,
+    screenshotOnRunFailure: true,
 
-    env: {
-        // codeCoverage: {
-        //     url: 'http://localhost:3000/__coverage__',
-        // },
-    }, projectId: '5r89t6', e2e: {
+    env: {}, projectId: '5r89t6', e2e: {
         setupNodeEvents(on, config) {
+            // implement node event listeners here
             require('cypress-mochawesome-reporter/plugin')(on);
             on('before:run', async (details) => {
                 console.log('override before:run');
@@ -30,9 +27,6 @@ module.exports = defineConfig({
                 console.log('override after:run');
                 await afterRunHook();
             });
-            // implement node event listeners here
-            //require('@cypress/code-coverage/task')(on, config)
-            //on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
         },
     },
 });
