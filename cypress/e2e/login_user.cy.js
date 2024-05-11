e70f5f8e547aa7288bd509a7008226a6580909c1
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-11 10:24:45 +0600

    invalid login test

--- cypress/e2e/login_user.cy.js
+++ cypress/e2e/login_user.cy.js
@@ -3,16 +3,16 @@ describe('Login User', () => {
         cy.visit('http://automationexercise.com')
 
         cy.contains('Signup / Login').click()
-        cy.contains('Login to your account')
+        cy.contains('Login to your account').should('be.visible')
 
         // Replace '#email' and '#password' with the actual selectors for each input field
         cy.get('[data-qa="login-email"]').type('abu@gmail.com')
         cy.get('[data-qa="login-password"]').type('123456')
         cy.get('[data-qa="login-button"]').click()
 
-        cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(10) > a').contains('Logged in as Abu Bakkar')
+        cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(10) > a').contains('Logged in as Abu Bakkar').should('be.visible')
 
         cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(5) > a').click()
-        cy.contains('ACCOUNT DELETED!')
+        cy.get('[data-qa="account-deleted"]').contains('Account Deleted!')
     })
 })
