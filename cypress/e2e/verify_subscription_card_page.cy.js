4fb52e7d733961d58a31ef71f0e689f2481bfe93
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-05-12 14:14:15 +0600

    subscription check from cart page

--- cypress/e2e/verify_subscription_card_page.cy.js
+++ cypress/e2e/verify_subscription_card_page.cy.js
@@ -1,5 +1,5 @@
-describe('Verify Subscription in home page', () => {
-    it('Verify Subscription in home page', () => {
+describe('Verify Subscription in card page', () => {
+    it('Verify Subscription in card page', () => {
         // Step 1: Launch browser
         // Cypress automatically launches the browser
 
@@ -9,17 +9,20 @@ describe('Verify Subscription in home page', () => {
         // Step 3: Verify that home page is visible successfully
         cy.url().should('include', 'automationexercise');
 
-        // Step 4: Scroll down to footer
+        // Step 4: Click on 'Cart' button
+        cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(3) > a').click();
+
+        // Step 5: Scroll down to footer
         cy.get('footer').scrollIntoView();
 
-        // Step 5: Verify text 'SUBSCRIPTION'
+        // Step 6: Verify text 'SUBSCRIPTION'
         cy.get('footer').should('contain', 'Subscription');
 
-        // Step 6: Enter email address in input and click arrow button
+        // Step 7: Enter email address in input and click arrow button
         cy.get('#susbscribe_email').type('testuser@example.com');
         cy.get('button[type=submit]').click();
 
-        // Step 7: Verify success message 'You have been successfully subscribed!' is visible
+        // Step 8: Verify success message 'You have been successfully subscribed!' is visible
         cy.get('.alert-success').should('contain', 'You have been successfully subscribed!');
     });
 });
