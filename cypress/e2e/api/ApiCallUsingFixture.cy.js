6c33aa953ea7a36e1a149073fe030e973fdbe256
ABS RAKIB <abu35-1994@diu.edu.bd>
2024-06-26 13:15:20 +0530

    API Call

--- cypress/e2e/api/ApiCallUsingFixture.cy.js
+++ cypress/e2e/api/ApiCallUsingFixture.cy.js
@@ -1,16 +1,16 @@
-// describe("API Call UsingFixture", () => {
-//     it("POST request using Fixture", () => {
-//         cy.fixture("restData").then((data) => {
-//             cy.request({
-//                 method: "POST",
-//                 url: "https://jsonplaceholder.typicode.com/posts",
-//                 body: data[0]
-//             }).then((response) => {
-//                 expect(response.status).to.eq(201)
-//                 expect(response.body).to.have.property('id')
-//                 expect(response.body).to.have.property('title')
-//                 expect(response.body).to.have.property('body')
-//             })
-//         })
-//     })
-// })
+describe("API Call UsingFixture", () => {
+    it("POST request using Fixture", () => {
+        cy.fixture("restData").then((data) => {
+            cy.request({
+                method: "POST",
+                url: "https://jsonplaceholder.typicode.com/posts",
+                body: data[0]
+            }).then((response) => {
+                expect(response.status).to.eq(201)
+                expect(response.body).to.have.property('id')
+                expect(response.body).to.have.property('title')
+                expect(response.body).to.have.property('body')
+            })
+        })
+    })
+})
